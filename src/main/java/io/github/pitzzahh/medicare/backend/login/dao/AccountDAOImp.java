9eece5cf068b4e9848b8e47016036ee83945e207
pitzzahh <pitzzahh@gmail.com>
2023-01-13 09:00:41 +0800

    add: dao implementation

---
+++ src/main/java/io/github/pitzzahh/medicare/backend/login/dao/AccountDAOImp.java
@@ -0,0 +1,49 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 pitzzahh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.pitzzahh.medicare.backend.login.dao;
+
+import static io.github.pitzzahh.medicare.backend.db.DatabaseConnection.getJDBC;
+import io.github.pitzzahh.medicare.backend.login.mapper.AccountMapper;
+import io.github.pitzzahh.medicare.backend.login.model.Account;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.Map;
+
+public class AccountDAOImp implements AccountDAO {
+
+    /**
+     * Get the accounts from the db as a map
+     * @return a {@code Map<String, Account>}
+     * @see Map
+     * @see Account
+     */
+    @Override
+    public Map<String, Account> getAccounts() {
+        return getJDBC().query("SELECT * FROM login", new AccountMapper())
+                .stream()
+                .collect(Collectors.toMap(Account::getUsername, Function.identity()));
+    }
+
+}
