ff94ceb0ae616845942864daad3348a251449657
pitzzahh <pitzzahh@gmail.com>
2023-01-13 10:29:58 +0800

    add: authentication logic (not yet real)

--- src/main/java/io/github/pitzzahh/medicare/controllers/AuthenticationController.java
+++ src/main/java/io/github/pitzzahh/medicare/controllers/AuthenticationController.java
@@ -24,5 +24,105 @@
 
 package io.github.pitzzahh.medicare.controllers;
 
+import static io.github.pitzzahh.medicare.util.WindowUtil.getParent;
+import static io.github.pitzzahh.medicare.util.WindowUtil.getStage;
+import io.github.pitzzahh.medicare.backend.login.cache.AuthData;
+import io.github.pitzzahh.medicare.backend.login.model.Account;
+import io.github.pitzzahh.medicare.util.ComponentUtil;
+import io.github.pitzzahh.medicare.util.WindowUtil;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.Parent;
+import javafx.fxml.FXML;
+import java.util.Map;
+
 public class AuthenticationController {
+
+    @FXML
+    public TextField username;
+
+    @FXML
+    public PasswordField password;
+
+    @FXML
+    public Button loginButton;
+
+    @FXML
+    public Label errorMessage;
+
+    @FXML
+    public void initialize() {
+        loginButton.setTooltip(new Tooltip("Click to log in"));
+    }
+
+    @FXML
+    public void onLogin(ActionEvent actionEvent) {
+        actionEvent.consume();
+        String username = this.username.getText();
+        String password = this.password.getText();
+        checkCredentials(username, password);
+    }
+
+    // TODO: add progress bar or some loading
+    private void checkCredentials(String username, String password) {
+        signIn(username, password);
+    }
+
+    private void signIn(String username, String password) {
+        Map<String, Account> accounts = AuthData.getAccounts();
+
+        //noinspection ConstantValue
+        if (true) { // !accounts.isEmpty()
+            boolean containsUsername = accounts.containsKey(username);
+            //noinspection ConstantValue
+            if (true) { // containsUsername
+                Account account = accounts.get(username);
+                //noinspection ConstantValue
+                if (true) { // account.getPassword().equals(password)
+                    this.username.clear();
+                    this.password.clear();
+                    errorMessage.setText("");
+                    Parent parent = getParent("main_panel");
+                    WindowUtil.loadParent(parent, "MEDiCARE");
+                    getStage().show();
+                }
+                else {
+                    ComponentUtil.getMainProgressBar(getParent("login_window")).ifPresent(p -> p.setVisible(false));
+                    errorMessage.setText("Invalid username or password");
+                }
+            } else {
+                ComponentUtil.getMainProgressBar(getParent("login_window")).ifPresent(p -> p.setVisible(false));
+                errorMessage.setText("Invalid username or password");
+            }
+        }
+        else {
+            ComponentUtil.getMainProgressBar(getParent("login_window")).ifPresent(p -> p.setVisible(false));
+            errorMessage.setText("Invalid username or password");
+        }
+    }
+
+    @FXML
+    public void onType(KeyEvent keyEvent) {
+        keyEvent.consume();
+
+        String username = this.username.getText();
+        String password = this.password.getText();
+
+        this.password.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ENTER)) checkCredentials(username, password);
+            else {
+                final String STYLE =
+                        "    -fx-border-color: transparent transparent #000 transparent;" +
+                                "    -fx-border-width: 2px;" +
+                                "    -fx-border-style: solid;" +
+                                "    -fx-font-size: 18px;";
+                this.username.setStyle(STYLE);
+                this.password.setStyle(STYLE);
+                this.errorMessage.setText("");
+            }
+        });
+
+    }
 }
