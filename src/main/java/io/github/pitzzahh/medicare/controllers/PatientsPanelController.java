9060ffbe1b7d47d30c1cdbf393db746d6cefdba6
pitzzahh <pitzzahh@gmail.com>
2023-01-15 10:26:26 +0800

    add: removing children of card storage

--- src/main/java/io/github/pitzzahh/medicare/controllers/PatientsPanelController.java
+++ src/main/java/io/github/pitzzahh/medicare/controllers/PatientsPanelController.java
@@ -42,6 +42,7 @@ public class PatientsPanelController {
     public void onViewPatients(ActionEvent actionEvent) {
         actionEvent.consume();
         loadPage("patients_panel", "view_patient");
+        getCardStorage().getChildren().removeAll();
         initPatientCards(getCardStorage());
     }
 }
