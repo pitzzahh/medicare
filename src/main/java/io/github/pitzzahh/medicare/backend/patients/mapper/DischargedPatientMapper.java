d87dc5f715b17465591caa47f1c884991cf1c435
pitzzahh <pitzzahh@gmail.com>
2023-01-25 23:13:56 +0800

    refactor: result set

--- src/main/java/io/github/pitzzahh/medicare/backend/patients/mapper/DischargedPatientMapper.java
+++ src/main/java/io/github/pitzzahh/medicare/backend/patients/mapper/DischargedPatientMapper.java
@@ -15,9 +15,9 @@ public class DischargedPatientMapper implements RowMapper<DischargedPatient> {
         final String[] DATE_CONFINED = decrypt(rs.getString("date_confined")).split("-");
         final String[] DATE_DISCHARGED = decrypt(rs.getString("date_discharged")).split("-");
         return new DischargedPatient(
-                Integer.parseInt(decrypt(rs.getString("id"))),
+                rs.getInt("id"),
                 decrypt(rs.getString("patient_name")),
-                decrypt(rs.getString("name_of_doctor")),
+                rs.getString("name_of_doctor").isEmpty() ? "" : decrypt(rs.getString("name_of_doctor")),
                 LocalDate.of(parseInt(DATE_CONFINED[0]), parseInt(DATE_CONFINED[1]), parseInt(DATE_CONFINED[2])),
                 LocalDate.of(parseInt(DATE_DISCHARGED[0]), parseInt(DATE_DISCHARGED[1]), parseInt(DATE_DISCHARGED[2]))
         );
