0d958e6ed3c4a34bfea0c93821b52fc02af6ba0d
pitzzahh <pitzzahh@gmail.com>
2023-01-27 00:27:41 +0800

    refactor: list type

--- src/main/java/io/github/pitzzahh/medicare/util/DataUtil.java
+++ src/main/java/io/github/pitzzahh/medicare/util/DataUtil.java
@@ -1,14 +1,14 @@
 package io.github.pitzzahh.medicare.util;
 
-import io.github.pitzzahh.medicare.backend.patients.model.DischargedPatient;
+import io.github.pitzzahh.medicare.backend.patients.model.Patient;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class DataUtil {
 
-    private static ObservableList<DischargedPatient> dataSource = FXCollections.observableArrayList();
+    private static final ObservableList<Patient> DATA_SOURCE = FXCollections.observableArrayList();
 
-    public static ObservableList<DischargedPatient> getDataSource() {
-        return dataSource;
+    public static ObservableList<Patient> getDataSource() {
+        return DATA_SOURCE;
     }
 }
