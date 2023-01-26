b2cf4dd3a17fd77f2132c24c58023d3f3b7d3bb2
pitzzahh <pitzzahh@gmail.com>
2023-01-27 00:30:03 +0800

    add: module attr

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,10 +8,11 @@ module medicare {
     requires lombok;
     requires util.classes;
 
+    opens io.github.pitzzahh.medicare.backend to javafx.base;
     opens io.github.pitzzahh.medicare.controllers to javafx.fxml;
     opens io.github.pitzzahh.medicare.application to javafx.graphics;
-    opens io.github.pitzzahh.medicare.controllers.patients to javafx.fxml;
     opens io.github.pitzzahh.medicare.controllers.doctors to javafx.fxml;
+    opens io.github.pitzzahh.medicare.controllers.patients to javafx.fxml;
     opens io.github.pitzzahh.medicare.backend.patients.model to javafx.base;
     exports io.github.pitzzahh.medicare;
     exports io.github.pitzzahh.medicare.backend.login.dao;
