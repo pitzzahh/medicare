57cc4a688c0cd1e52d9d1038fc98270d32861288
pitzzahh <pitzzahh@gmail.com>
2023-01-27 00:28:27 +0800

    add: more table column cell value

--- src/main/java/io/github/pitzzahh/medicare/controllers/DischargeController.java
+++ src/main/java/io/github/pitzzahh/medicare/controllers/DischargeController.java
@@ -1,7 +1,7 @@
 package io.github.pitzzahh.medicare.controllers;
 
 import static io.github.pitzzahh.medicare.util.ComponentUtil.initTableColumns;
-import io.github.pitzzahh.medicare.backend.patients.model.DischargedPatient;
+import io.github.pitzzahh.medicare.backend.patients.model.Patient;
 import io.github.pitzzahh.medicare.util.DataUtil;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
@@ -13,24 +13,24 @@ import java.net.URL;
 public class DischargeController implements Initializable {
 
     @FXML
-    private TableView<DischargedPatient> table;
+    private TableView<Patient> table;
 
-    private static TableView<DischargedPatient> copy;
+    private static TableView<Patient> copy;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         copy = table;
     }
 
-    public static void setData(ObservableList<DischargedPatient> data) {
+    public static void setData(ObservableList<Patient> data) {
         DataUtil.getDataSource()
                 .addAll(data);
-        initTableColumns(copy, new String[]{"patientId", "name", "nameOfDoctor", "dateConfined", "dateDischarged"});
+        initTableColumns(copy, new String[]{"patientId", "lastName", "firstName", "middleName", "symptoms", "assignedDoctor", "dateConfined", "dateDischarged"});
         copy.setItems(DataUtil.getDataSource());
         copy.getSelectionModel().clearSelection();
     }
 
-    public static TableView<DischargedPatient> getCopy() {
+    public static TableView<Patient> getCopy() {
         return copy;
     }
 }
